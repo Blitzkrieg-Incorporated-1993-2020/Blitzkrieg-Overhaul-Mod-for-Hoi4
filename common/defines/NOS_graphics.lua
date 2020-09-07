3dc2266c5e3efba8aabb6cce2a32db534f6e4d2a
Alithron <alithron100@gmail.com>
2020-09-07 17:41:15 +0100

    The Merged Common File for BKG
    Common file done, more 2 come

---
+++ common/defines/NOS_graphics.lua
@@ -0,0 +1,20 @@
+
+NDefines_Graphics.NMapIcons.STATES_PRIORITY_VICTORY_POINTS = 8
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 1.0 -- thickness in pixels -- 5
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 0.5 -- The center gradient is linear 1/255 per pixel for this many pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 15.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 45.0 --11
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 2.0 --2
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 15.0 --20
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 50.0 --20
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_STRATEGIC_REGIONS = 0.973
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0 -- 0.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 1.0 -- 0.6
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 1.0
+
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_COUNTRY = 0.98
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 1.25
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_NEAR = 350
+NDefines_Graphics.NGraphics.PROVINCE_BORDER_FADE_FAR = 355
+NDefines_Graphics.NMapMode.FACTIONS_COLOR_NOT_MEMBER = { 0.5, 0.5, 0.5, 0.1 } -- 0.6, 0.6, 0.6, 0.25
+
